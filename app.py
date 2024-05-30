b3c321d26cb53e3e7f95590d0e1680746e1f0542
scooter7 <scooter.vineburgh@gmail.com>
2024-05-30 17:29:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from GoogleNews import GoogleNews
 import tweepy
-from linkedin_api import Linkedin
 import json
 
 twitter_api_key = st.secrets["twitter"]["api_key"]
@@ -9,13 +8,9 @@ twitter_api_secret = st.secrets["twitter"]["api_secret"]
 twitter_access_token = st.secrets["twitter"]["access_token"]
 twitter_access_token_secret = st.secrets["twitter"]["access_token_secret"]
 
-linkedin_cookies = json.loads(st.secrets["linkedin"]["cookies"])
-
 auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
 auth.set_access_token(twitter_access_token, twitter_access_token_secret)
 twitter_api = tweepy.API(auth)
-
-linkedin_api = Linkedin(username=None, password=None, cookies=linkedin_cookies)
 
 googlenews = GoogleNews()
 
@@ -28,8 +23,6 @@ if keyword:
     google_news_stories = googlenews.results()
 
     tweets = tweepy.Cursor(twitter_api.search_tweets, q=keyword, lang='en').items(10)
-    
-    linkedin_posts = linkedin_api.search_posts(keyword)
 
     st.header('Google News Stories')
     for article in google_news_stories:
@@ -41,9 +34,3 @@ if keyword:
     for tweet in tweets:
         st.subheader(f"@{tweet.user.screen_name}")
         st.write(tweet.text)
-
-    st.header('LinkedIn Posts')
-    for post in linkedin_posts:
-        st.subheader(post['title'])
-        st.write(post['description'])
-        st.write(f"[Read more]({post['url']})")
